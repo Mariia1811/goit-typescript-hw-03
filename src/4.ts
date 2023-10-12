b917ce82d04a85f0b24da88700cd34ba12a0e0a2
Mariia Kryzhalko <kruzhalko93@gmail.com>
2023-10-12 14:47:55 +0300

    task 03 done

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,43 @@
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private keyObj: Key) {}
+  getKey(): Key {
+    return this.keyObj;
+  }
+}
+
+abstract class House {
+  protected door = false;
+  private tenants: Person[] = [];
+
+  constructor(protected keyObj: Key) {}
+
+  comeIn(person: Person): void {
+    if (this.door) this.tenants.push(person);
+  }
+
+  abstract openDoor(keyObj: Key): boolean;
+}
+
+class MyHouse extends House {
+  openDoor(keyObj: Key) {
+    if (keyObj.getSignature() !== this.keyObj.getSignature()) {
+      return (this.door = true);
+    }
+    return (this.door = false);
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +46,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
